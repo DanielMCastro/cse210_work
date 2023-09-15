a74fc79771cbe4a15632a5a4cbc5d3a1145e3587
Daniel Castro <dmcastro8@gmail.com>
2023-09-15 14:42:41 -0600

    Finished the letter grade assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percent for the class?");
+        string answer = Console.ReadLine();
+        int gradePercent = int.Parse(answer);
+        string letter = "";
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else if (gradePercent < 60)
+        {
+            letter = "F";  
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+        if (letter == "D" || letter == "F")
+        {
+            Console.WriteLine("I'm sorry, you did not pass the class. Try again!");
+        }
+        else
+        {
+            Console.WriteLine("You passed the class. Congratulations!");
+        }
     }
 }
