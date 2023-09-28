ed2c9f652399e73fe404ab39a6f5d41dac8f970c
Daniel Castro <dmcastro8@gmail.com>
2023-09-27 20:33:14 -0600

    commiting W03 Prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        {
+            job1.company = "Tidal";
+            job1.jobTitle = "Software Engineer";
+            job1.startYear = 2007;
+            job1.endYear = 2019;
+        }
+        Job job2 = new Job();
+        {
+            job2.company = "Spotify";
+            job2.jobTitle = "Software Engineer";
+            job2.startYear = 2010;
+            job2.endYear = 2012;
+        }
+
+        Resume resume = new Resume();
+        {
+            resume.jobs.Add(job1);
+            resume.jobs.Add(job2);
+        }
+
+        resume.Display();
     }
 }
